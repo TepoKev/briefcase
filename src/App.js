72bf2d9da70746715f1627bf5648d9b8b2144a24
TepoKev <tepokev@gmail.com>
2020-09-14 22:56:10 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/app.css';
+import Gear from "./components/Gear";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <h1 className="title">
+        Many new features. Coming soon.
+      </h1>
+      <Gear />
     </div>
   );
 }
